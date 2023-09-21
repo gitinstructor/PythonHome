fbc3ec605653a276252f368efd3fdbcf6e793ac1
henry <javaexpert@nate.com>
2023-09-21 12:27:06 +0900

    성적관리프로그램 실습

---
+++ SungjukMgmt/student_output.py
@@ -0,0 +1,6 @@
+#student_output.py
+
+def output(student) : 
+    print(f"{student['name']}\t{student['kor']}\t{student['eng']}",end='\t')
+    print(f"{student['math']}\t{student['total']}\t{student['avg']:.2f}", end='\t')
+    print(f"{student['grade']}")
