6f56fe4406f5e5008b6ff68290b2c9ff6bea1a52
henry <javaexpert@nate.com>
2023-10-05 15:01:24 +0900

    10월 5일 수업소스

---
+++ 1005/demo.py
@@ -0,0 +1,21 @@
+import pymysql
+
+# 1. Connect
+conn, cursor = None, None
+try :
+    conn = pymysql.connect(host='43.202.46.175', port=3306, 
+                       user='root', password='pythonmysql')
+# 2. cursor
+    cursor = conn.cursor()
+# 3. SQL Statement
+    sql = "SELECT NOW()"
+    cursor.execute(sql)
+# 4. fetch
+    result = cursor.fetchone()  
+    print(result)  
+except Exception as err: 
+    print(err)
+finally : 
+# 5. close
+    cursor.close()
+    conn.close()
